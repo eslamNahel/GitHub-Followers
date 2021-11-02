1ac729e296d727fa8e77e2db9488b031807cd897
eslamNahel <eslamoz.nasser@gmail.com>
2021-11-02 22:29:14 +0200

    Add Model Files, fix nav bar bug

---
+++ GitHub Followers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHub Followers
+//
+//  Created by Eslam Nahel on 02/11/2021.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
