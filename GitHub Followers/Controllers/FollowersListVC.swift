8a88846c9960979291d45401902111fb11acdc6a
eslamNahel <eslamoz.nasser@gmail.com>
2021-11-03 23:42:36 +0200

    Configure Collection View Layout

--- GitHub Followers/Controllers/FollowersListVC.swift
+++ GitHub Followers/Controllers/FollowersListVC.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class FollowersListVC: UIViewController {
 
+    //MARK: - Properties
+    
     var userName: String!
     var collectionView: UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,8 @@ class FollowersListVC: UIViewController {
     }
     
     
+    //MARK: - Configuration Methods
+    
     private func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles  = true
@@ -32,11 +37,24 @@ class FollowersListVC: UIViewController {
 
     
     private func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createFlowLayoutColumns())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.resuseID)
     }
     
     
+    private func createFlowLayoutColumns() -> UICollectionViewFlowLayout {
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = availableWidth / 3
+        
+        let layout                      = UICollectionViewFlowLayout()
+        layout.sectionInset             = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        layout.itemSize                 = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return UICollectionViewFlowLayout()
+    }
 }
