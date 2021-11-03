46a801d4a20e9faea22d8109dac0018941956425
eslamNahel <eslamoz.nasser@gmail.com>
2021-11-03 17:58:52 +0200

    Add error messages as an enum

--- GitHub Followers/Managers/NetworkManager.swift
+++ GitHub Followers/Managers/NetworkManager.swift
@@ -14,28 +14,28 @@ class NetworkManager {
     
     private let baseURL = "https://api.github.com/users/"
     
-    func getFollowers(for username: String, page: Int, completion: @escaping ([Follower]?, String?) -> Void) {
+    func getFollowers(for username: String, page: Int, completion: @escaping ([Follower]?, ErrorMessage?) -> Void) {
         let endPoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endPoint) else {
-            completion(nil, "Invalid URL.")
+            completion(nil, .invalidUserName)
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             
             if let _ = error {
-                completion(nil, "Error getting data!")
+                completion(nil, .unableToComplete)
                 return
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completion(nil, "Something went wrong with the request! Try again")
+                completion(nil, .invalidResponse)
                 return
             }
             
             guard let data = data else {
-                completion(nil, "Something went wrong with the request! Try again")
+                completion(nil, .invalidData)
                 return
             }
 
@@ -45,7 +45,7 @@ class NetworkManager {
                 let followers = try decoder.decode([Follower].self, from: data)
                 completion(followers, nil)
             } catch {
-                completion(nil, "Error decoding the data!")
+                completion(nil, .invalidData)
             }
         }
         task.resume()
---
+++ GitHub Followers/Utilities/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  GitHub Followers
+//
+//  Created by Eslam Nahel on 03/11/2021.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUserName    = "This userName is invalid, please try again! 😃"
+    case unableToComplete   = "Opps! an error occurred, please try again! 🤷🏻‍♂️"
+    case invalidResponse    = "Something bad happened, please try again! 😅"
+    case invalidData        = "Couldn't complete the response, please try again! 🙄"
+}
+
