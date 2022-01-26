bf260d8899fa1ba73907322ac2b9d1f5d39d496f
eslamNahel <eslamoz.nasser@gmail.com>
2022-01-26 18:55:38 +0200

    Add: add persistanceManager and its main functions.

---
+++ GitHub Followers/Managers/PersistenceManager.swift
@@ -0,0 +1,77 @@
+//
+//  PersistenceManager.swift
+//  GitHub Followers
+//
+//  Created by Eslam Nahel on 26/01/2022.
+//
+
+import UIKit
+
+
+enum FavoritesManagerActionType {
+    case add
+    case remove
+}
+
+class PersistenceManager {
+    static let shared       = PersistenceManager()
+    private let defaults    = UserDefaults.standard
+    
+    private enum Keys {
+        static let favorites = "favorites"
+    }
+    
+    
+    func updateFavoritesList(with favorite: Follower, actionType: FavoritesManagerActionType, completed: (ErrorMessage?) -> Void) {
+        getFavorites { results in
+            switch results {
+            case .success(var savedFavorites):
+                switch actionType {
+                case .add:
+                    guard !savedFavorites.contains(favorite) else {
+                        completed(.alreadyInFavorites)
+                        return
+                    }
+                    
+                    savedFavorites.append(favorite)
+                case .remove:
+                    savedFavorites.removeAll { $0.login == favorite.login }
+                }
+                
+                completed(saveFavorites(favorites: savedFavorites))
+                
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
+    
+    func getFavorites(completed: (Result<[Follower], ErrorMessage>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completed(.success([]))
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.invalidToSaveFavorite))
+        }
+    }
+    
+    
+    private func saveFavorites(favorites: [Follower]) -> ErrorMessage? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.set(encodedFavorites, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .invalidToSaveFavorite
+        }
+    }
+    
+}
--- GitHub Followers/Utilities/ErrorMessage.swift
+++ GitHub Followers/Utilities/ErrorMessage.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 enum ErrorMessage: String, Error {
-    case invalidUserName    = "This userName is invalid, please try again! 😃"
-    case unableToComplete   = "Opps! an error occurred, please try again! 🤷🏻‍♂️"
-    case invalidResponse    = "Something bad happened, please try again! 😅"
-    case invalidData        = "Couldn't complete the response, please try again! 🙄"
+    case invalidUserName        = "This userName is invalid, please try again! 😃"
+    case unableToComplete       = "Opps! an error occurred, please try again! 🤷🏻‍♂️"
+    case invalidResponse        = "Something bad happened, please try again! 😅"
+    case invalidData            = "Couldn't complete the response, please try again! 🙄"
+    case invalidToSaveFavorite  = "Hey! sorry, we couldn't save this use to favorites! 🥺"
+    case alreadyInFavorites     = "wait what? this user is already in favorites list! 🙂"
 }
 
